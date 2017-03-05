5c0e9a223264ce700e8f87698cd7e623928d5f9c
leider <derleider@web.de>
2017-03-05 18:36:38 +0100

    Ausgabenseite fertig

---
+++ frontend/javascript/check-ausgabenform.js
@@ -0,0 +1,23 @@
+// THE ORIGINAL OF THIS FILE IS IN frontend/javascript
+
+var ausgaben_validator;
+(function () {
+  'use strict';
+
+  function initValidator() {
+
+    ausgaben_validator = $('#ausgabenform').validate({
+      errorPlacement: function (error, element) {
+        error.insertAfter(element);
+      },
+      errorElement: 'span',
+      errorClass: 'help-block',
+      highlight: function (element) { $(element).parent().addClass('has-error'); },
+      unhighlight: function (element) { $(element).parent().removeClass('has-error'); }
+    });
+
+    ausgaben_validator.form();
+  }
+
+  $(document).ready(initValidator);
+}());
