0bb85553c9a469a43c1374e61aa4046fb55f93c2
leider <derleider@web.de>
2019-01-04 20:24:48 +0100

    revert accidental change

--- migrateOrte.js
+++ migrateOrte.js
@@ -3,11 +3,32 @@
 'use strict';
 
 require('./configure');
-require('./initWinston');
-const optionenstore = require('./lib/mailsender/sendMailsNightly.js');
+const beans = require('simple-configure').get('beans');
+const optionenstore = beans.get('optionenstore');
+const Orte = beans.get('orte');
 
-optionenstore.checkFluegel(
-  () => {
+optionenstore.get((err1, optionen) => {
+  if (err1) {
+    console.log(err1);
     process.exit();
   }
-);
+  if (!optionen.state.flaechen) {
+    console.log('nix zu migrieren');
+    process.exit();
+  }
+  const orte = new Orte();
+  optionen.state.flaechen.forEach(each => {
+    orte.addOrt({name: each.ort, flaeche: each.flaeche, pressename: each.ort, presseIn: each.ort});
+  });
+  optionenstore.save(orte, err => {
+    if (err) {
+      console.log(err);
+    }
+    optionenstore.save(optionen, err2 => {
+      if (err2) {
+        console.log(err2);
+      }
+      process.exit();
+    });
+  });
+});
