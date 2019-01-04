eebe3ee9375a65107cdca0aa55e3ca8b1ffe257a
leider <derleider@web.de>
2019-01-04 19:34:17 +0100

    Fluegelmails vorbereitet

--- migrateOrte.js
+++ migrateOrte.js
@@ -3,32 +3,11 @@
 'use strict';
 
 require('./configure');
-const beans = require('simple-configure').get('beans');
-const optionenstore = beans.get('optionenstore');
-const Orte = beans.get('orte');
+require('./initWinston');
+const optionenstore = require('./lib/mailsender/sendMailsNightly.js');
 
-optionenstore.get((err1, optionen) => {
-  if (err1) {
-    console.log(err1);
+optionenstore.checkFluegel(
+  () => {
     process.exit();
   }
-  if (!optionen.state.flaechen) {
-    console.log('nix zu migrieren');
-    process.exit();
-  }
-  const orte = new Orte();
-  optionen.state.flaechen.forEach(each => {
-    orte.addOrt({name: each.ort, flaeche: each.flaeche, pressename: each.ort, presseIn: each.ort});
-  });
-  optionenstore.save(orte, err => {
-    if (err) {
-      console.log(err);
-    }
-    optionenstore.save(optionen, err2 => {
-      if (err2) {
-        console.log(err2);
-      }
-      process.exit();
-    });
-  });
-});
+);
