b8a5eb8abbdc58dd2a8ec98edacf7f305b8a6952
leider <derleider@web.de>
2020-10-26 17:34:25 +0100

    add some forgotten plugins

--- vue/plugins/bootstrap-vue.ts
+++ vue/plugins/bootstrap-vue.ts
@@ -10,12 +10,15 @@ import { FormDatepickerPlugin } from "bootstrap-vue/src/components/form-datepick
 import { FormCheckboxPlugin } from "bootstrap-vue/src/components/form-checkbox";
 import { FormFilePlugin } from "bootstrap-vue/src/components/form-file";
 import { FormTextareaPlugin } from "bootstrap-vue/src/components/form-textarea";
+import { InputGroupPlugin } from "bootstrap-vue/src/components/input-group";
 
 import { CollapsePlugin } from "bootstrap-vue/src/components/collapse";
 
 import { ModalPlugin } from "bootstrap-vue/src/components/modal";
 import { TabsPlugin } from "bootstrap-vue/src/components/tabs";
 import { TooltipPlugin } from "bootstrap-vue/src/components/tooltip";
+import { OverlayPlugin } from "bootstrap-vue/src/components/overlay";
+import { LinkPlugin } from "bootstrap-vue/src/components/link";
 
 import { BFormInvalidFeedback } from "bootstrap-vue/src/components/form";
 
@@ -29,10 +32,13 @@ Vue.use(FormDatepickerPlugin);
 Vue.use(FormCheckboxPlugin);
 Vue.use(FormFilePlugin);
 Vue.use(FormTextareaPlugin);
+Vue.use(InputGroupPlugin);
 Vue.use(CollapsePlugin);
 Vue.use(ModalPlugin);
 Vue.use(TabsPlugin);
 Vue.use(IconsPlugin);
 Vue.use(TooltipPlugin);
+Vue.use(OverlayPlugin);
+Vue.use(LinkPlugin);
 
 Vue.component("BFormInvalidFeedback", BFormInvalidFeedback);
