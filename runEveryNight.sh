a2236d3ad19ee589fdc83e96e6ab1657bc8cae80
leider <derleider@web.de>
2021-10-25 17:02:48 +0200

    update "node" location in runEveryNight.sh

--- runEveryNight.sh
+++ runEveryNight.sh
@@ -1,1 +1,1 @@
-cd /home/andreas/JC-Backoffice/ && /usr/bin/node --icu-data-dir=node_modules/full-icu batchjobs/src/index.js
+cd /home/andreas/JC-Backoffice/ && /usr/local/bin/node --icu-data-dir=node_modules/full-icu batchjobs/src/index.js
