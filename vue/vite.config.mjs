a85a0627aaa27c3820de25bee514720410161d19
leider <derleider@web.de>
2023-03-29 12:05:11 +0200

    fix build (to not start dev server there)

--- vue/vite.config.mjs
+++ vue/vite.config.mjs
@@ -2,30 +2,32 @@ import { defineConfig } from "vite";
 // import vue from '@vitejs/plugin-vue' // vue 3
 import { createVuePlugin as vue } from "vite-plugin-vue2"; //vue 2
 
-import "jc-backend/configure";
-import configureApp from "jc-backend/configureApp";
 import path from "path";
 import express from "express";
 // https://vitejs.dev/config/
-export default defineConfig({
-  root: __dirname,
-  base: "/vue/",
-  build: { outDir: "../backend/static/vue", emptyOutDir: true },
-  plugins: [
-    vue(),
-    {
-      name: "vite-plugin-cache-control",
-      configureServer(server) {
-        const app = express()
-        configureApp(app, true);
-        server.middlewares.use(app);
+export default defineConfig(() => {
+  return {
+    root: __dirname,
+    base: "/vue/",
+    build: { outDir: "../backend/static/vue", emptyOutDir: true },
+    plugins: [
+      vue(),
+      {
+        name: "vite-plugin-cache-control",
+        async configureServer(server) {
+          await import("jc-backend/configure");
+          const configureApp = await import("jc-backend/configureApp");
+          const app = express();
+          configureApp.default(app, true);
+          server.middlewares.use(app);
+        },
       },
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+      extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json", ".vue"],
     },
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-    extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json", ".vue"],
-  },
+  };
 });
