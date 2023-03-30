538666fabb878287d00717ced1f71f989e782a91
leider <derleider@web.de>
2023-03-30 22:13:54 +0200

    update cron script

--- runEveryNight.sh
+++ runEveryNight.sh
@@ -1,1 +1,12 @@
-cd /home/andreas/JC-Backoffice/ && /usr/local/bin/node --icu-data-dir=node_modules/full-icu batchjobs/src/index.js
+#!/bin/sh
+cd /home/andreas
+
+exec 2>&1
+export USER=andreas
+export HOME=/home/andreas
+export NODE_ENV=production
+export NODE_ICU_DATA=$HOME/JC-Backoffice/node_modules/full-icu
+# Include the user-specific profile
+. $HOME/.profile
+# exec node --icu-data-dir=/home/andreas/JC-Backoffice/node_modules/full-icu JC-Backoffice/start.js
+exec node JC-Backoffice/batchjobs/src/index.js
