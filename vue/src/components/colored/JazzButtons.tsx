a2eeccc6c822c50247df27e8e492b34e6e856d71
leider <derleider@web.de>
2023-05-29 13:54:47 +0200

    Refactoring: Extract Delete Function To Button

--- vue/src/components/colored/JazzButtons.tsx
+++ vue/src/components/colored/JazzButtons.tsx
@@ -1,6 +1,8 @@
-import { Button, ConfigProvider } from "antd";
+import { App, Button, ConfigProvider } from "antd";
 import { IconForSmallBlock } from "@/components/Icon";
 import * as React from "react";
+import { useNavigate } from "react-router-dom";
+import { deleteVeranstaltungWithId } from "@/commons/loader-for-react";
 
 type ButtonProps = {
   callback?: () => void;
@@ -16,7 +18,21 @@ export function SaveButton({ callback, disabled }: ButtonProps) {
   );
 }
 
-export function DeleteButton({ callback, disabled }: ButtonProps) {
+export function DeleteButton({ disabled }: ButtonProps) {
+  const { modal } = App.useApp();
+  const navigate = useNavigate();
+  function callback() {
+    modal.confirm({
+      type: "confirm",
+      title: "Veranstaltung löschen",
+      content: `Bist Du sicher, dass Du die Veranstaltung "${document.title}" löschen möchtest?`,
+      onOk: async () => {
+        await deleteVeranstaltungWithId(id);
+        navigate("/");
+      },
+    });
+  }
+
   return (
     <ConfigProvider theme={{ token: { colorPrimary: "#dc3545" } }}>
       <Button icon={<IconForSmallBlock iconName="Trash" />} type="primary" disabled={disabled} onClick={callback}>
