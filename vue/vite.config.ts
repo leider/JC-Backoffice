4724c98cbcd58cbbca19cf5cd027af82838c74b4
leider <derleider@web.de>
2023-09-30 18:09:27 +0200

    try pwa

--- vue/vite.config.ts
+++ vue/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig, splitVendorChunkPlugin } from "vite";
 import react from "@vitejs/plugin-react";
 
+import { VitePWA } from "vite-plugin-pwa";
 // @ts-ignore
 import path, { resolve } from "path";
 // @ts-ignore
@@ -32,6 +33,7 @@ export default defineConfig(() => {
           server.middlewares.use(app);
         },
       },
+      VitePWA({ registerType: "autoUpdate" }),
     ],
     resolve: {
       alias: {
