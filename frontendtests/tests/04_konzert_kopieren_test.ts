4c4bf39e4983a399734ccc4b78d12c58991f5906
leider <derleider@web.de>
2024-11-01 16:35:03 +0100

    fix Kopieren Test

--- frontendtests/tests/04_konzert_kopieren_test.ts
+++ frontendtests/tests/04_konzert_kopieren_test.ts
@@ -9,7 +9,8 @@ Before(({ I, login }) => {
 
 Scenario("Kopiere Konzert", async ({ I }) => {
   I.amOnPage("/vue/konzert/copy-of-Kopiervorlage?page=allgemeines");
+  I.fillField("Titel", "Kopiertes Konzert");
   I.click("Speichern");
   I.click("Veranstaltungen");
-  I.see("Kopie von Kopiervorlage");
+  I.see("Kopiertes Konzert");
 });
