9941b67d392854d61fbe950c918f8628bda31d0b
leider <derleider@web.de>
2024-11-23 12:52:37 +0100

    tweak steps

--- frontendtests/tests/04_konzert_kopieren_test.ts
+++ frontendtests/tests/04_konzert_kopieren_test.ts
@@ -9,6 +9,7 @@ Before(({ I, login }) => {
 
 Scenario("Kopiere Konzert", async ({ I }) => {
   I.amOnPage("/vue/konzert/copy-of-Kopiervorlage?page=allgemeines");
+  I.wait(0.5);
   I.fillField("Titel", "Kopiertes Konzert");
   I.click("Speichern");
   I.click("Veranstaltungen");
