41261653e2ccf40d986dbbfcc92da5cfb37f1a22
leider <derleider@web.de>
2024-11-24 12:11:20 +0100

    Revert "tweak steps"
    This reverts commit 9941b67d392854d61fbe950c918f8628bda31d0b.

--- frontendtests/tests/04_konzert_kopieren_test.ts
+++ frontendtests/tests/04_konzert_kopieren_test.ts
@@ -9,7 +9,6 @@ Before(({ I, login }) => {
 
 Scenario("Kopiere Konzert", async ({ I }) => {
   I.amOnPage("/vue/konzert/copy-of-Kopiervorlage?page=allgemeines");
-  I.wait(0.5);
   I.fillField("Titel", "Kopiertes Konzert");
   I.click("Speichern");
   I.click("Veranstaltungen");
