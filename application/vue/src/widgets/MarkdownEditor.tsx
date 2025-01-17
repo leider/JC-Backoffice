6b1d79e3cd39c55a85b92ca2b96c2f0907e73883
leider <derleider@web.de>
2025-01-17 21:11:11 +0100

    1st running version

--- application/vue/src/widgets/MarkdownEditor.tsx
+++ application/vue/src/widgets/MarkdownEditor.tsx
@@ -1,6 +1,23 @@
 import { Form } from "antd";
-import React, { ReactNode } from "react";
-import { Editable, useEditor } from "@wysimark/react";
+import React, { ReactNode, useEffect } from "react";
+import "@mdxeditor/editor/style.css";
+import {
+  BlockTypeSelect,
+  BoldItalicUnderlineToggles,
+  diffSourcePlugin,
+  DiffSourceToggleWrapper,
+  headingsPlugin,
+  InsertThematicBreak,
+  linkPlugin,
+  listsPlugin,
+  ListsToggle,
+  MDXEditor,
+  MDXEditorMethods,
+  quotePlugin,
+  thematicBreakPlugin,
+  toolbarPlugin,
+  UndoRedo,
+} from "@mdxeditor/editor";
 
 export function MarkdownEditor({ label, name }: { label?: string | ReactNode; name: string[] | string }) {
   return (
@@ -10,6 +27,40 @@ export function MarkdownEditor({ label, name }: { label?: string | ReactNode; na
   );
 }
 function InnerEditor({ value, onChange }: { value?: string; onChange?: (value: string) => void }) {
-  const editor = useEditor({ authToken: "" });
-  return <Editable editor={editor} value={value ?? ""} onChange={onChange!} />;
+  const mdxEditorRef = React.useRef<MDXEditorMethods>(null);
+
+  useEffect(() => {
+    if (value) {
+      mdxEditorRef.current?.setMarkdown(value);
+    }
+  }, [value]);
+
+  return (
+    <MDXEditor
+      ref={mdxEditorRef}
+      markdown={""}
+      onChange={onChange}
+      plugins={[
+        toolbarPlugin({
+          toolbarClassName: "my-classname",
+          toolbarContents: () => (
+            <DiffSourceToggleWrapper>
+              <UndoRedo />
+              <BlockTypeSelect />
+              <BoldItalicUnderlineToggles />
+              <ListsToggle />
+              <InsertThematicBreak />
+            </DiffSourceToggleWrapper>
+          ),
+        }),
+        listsPlugin(),
+        quotePlugin(),
+        linkPlugin(),
+        diffSourcePlugin({ viewMode: "rich-text" }),
+        headingsPlugin(),
+        listsPlugin(),
+        thematicBreakPlugin(),
+      ]}
+    />
+  );
 }
