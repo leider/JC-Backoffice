dd6088c3a2b36c5310aef56ba7de1cc6fb25d75a
Andreas Leidig <derleider@web.de>
2025-02-05 22:33:35 +0100

    better excel (#31)

--- application/shared/excelPreparation/excelFormatters.ts
+++ application/shared/excelPreparation/excelFormatters.ts
@@ -2,128 +2,147 @@ import Konzert from "../konzert/konzert.js";
 import KonzertKalkulation from "../konzert/konzertKalkulation.js";
 import Vermietung from "../vermietung/vermietung.js";
 import OptionValues from "../optionen/optionValues.js";
+import Veranstaltung from "../veranstaltung/veranstaltung.js";
 
 export function unifySpende(name: string) {
   const spendeVariations = ["spende", "spenden", "Spende", "Spenden"];
   return spendeVariations.includes(name) ? "Spende" : name;
 }
 
-export function createExcelData({ konzert, optionen }: { konzert: Konzert; optionen: OptionValues }) {
-  const kasse = konzert.kasse;
-  const kosten = konzert.kosten;
-  const staff = konzert.staff;
-  const kalk = new KonzertKalkulation(konzert);
-  const klavierStimmerStandard = konzert.technik.fluegel ? optionen.preisKlavierstimmer : 0;
+export type SimpleExcelRow = {
+  Art: string;
+  Einnahme?: number;
+  Ausgabe?: number | "N/A";
+};
 
-  const titelZeile = { Art: konzert.kopf.titel + " - " + konzert.startDatumUhrzeit.mitUhrzeitNumerisch };
-  // Einnahmen
-  const eintrittRow = {
-    Art: `Eintritt (${kasse.istFreigegeben ? "" : "nicht "}freigegeben)`,
-    Einnahme: kasse.einnahmeTicketsEUR ?? 0,
-    Ausgabe: "",
-  };
-  const reservixRow = {
-    Art: "Eintritt Reservix",
-    Einnahme: kasse.einnahmenReservix ?? 0,
-    Ausgabe: "",
-  };
-  const barEinnahmenRow = { Art: "Bar Einnahmen", Einnahme: kasse.einnahmeOhneBankUndTickets ?? 0, Ausgabe: "" };
-  const barEinlageRow = { Art: "Bar Einlage", Einnahme: kasse.einnahmeBankEUR ?? 0, Ausgabe: "" };
-  const zuschussRow = { Art: "Zuschüsse", Einnahme: konzert.eintrittspreise.zuschuss ?? 0, Ausgabe: "" };
-  // Ausgaben
-  const barAusgabenRow = { Art: "Barausgaben", Einnahme: "", Ausgabe: kasse.ausgabenOhneGage ?? 0 };
-  const barAnBankRow = { Art: "Bar an Bank", Einnahme: "", Ausgabe: kasse.ausgabeBankEUR ?? 0 };
+export function createExcelData({ veranstaltung, optionen }: { veranstaltung: Veranstaltung; optionen: OptionValues }) {
+  const klavierStimmerStandard = veranstaltung.technik.fluegel ? optionen.preisKlavierstimmer : 0;
 
-  const gagenRow = { Art: "Gagen", Einnahme: "", Ausgabe: kosten.gagenTotalEUR ?? 0 };
-  const dealRow = { Art: "Gagen (Deal)", Einnahme: "", Ausgabe: kalk.dealAbsolutEUR ?? 0 };
-  const provisionRow = { Art: "Provision Agentur", Einnahme: "", Ausgabe: kosten.provisionAgentur ?? 0 };
-  const backlineRockshopRow = { Art: "Backline Rockshop", Einnahme: "", Ausgabe: kosten.backlineEUR ?? 0 };
-  const technikZumietungRow = { Art: "Technik Zumietung", Einnahme: "", Ausgabe: kosten.technikAngebot1EUR ?? 0 };
-  const fluegelStimmerRow = { Art: "Flügelstimmer", Einnahme: "", Ausgabe: kosten.fluegelstimmerEUR || klavierStimmerStandard };
-  const saalmieteRow = { Art: "Saalmiete", Einnahme: "", Ausgabe: kosten.saalmiete ?? 0 };
-  const werbung1Row = { Art: kosten.werbung1Label, Einnahme: "", Ausgabe: kosten.werbung1 ?? 0 };
-  const werbung2Row = { Art: kosten.werbung2Label, Einnahme: "", Ausgabe: kosten.werbung2 ?? 0 };
-  const werbung3Row = { Art: kosten.werbung3Label, Einnahme: "", Ausgabe: kosten.werbung3 ?? 0 };
-  const personalRow = { Art: "Personal (unbar)", Einnahme: "", Ausgabe: kosten.personal ?? 0 };
-  const tontechnikerRow = {
-    Art: "Tontechniker",
-    Einnahme: "",
-    Ausgabe: !staff.technikerVNotNeeded && !kosten.tontechniker ? "N/A" : kosten.tontechniker,
-  };
-  const lichttechnikerRow = {
-    Art: "Lichttechniker",
-    Einnahme: "",
-    Ausgabe: !staff.technikerNotNeeded && !kosten.lichttechniker ? "N/A" : kosten.lichttechniker,
-  };
-  const cateringMusikerRow = { Art: "Catering (Musiker)", Einnahme: "", Ausgabe: kosten.cateringMusiker ?? 0 };
-  const cateringPersonalRow = { Art: "Catering (Personal)", Einnahme: "", Ausgabe: kosten.cateringPersonal ?? 0 };
-  const hotelRow = { Art: "Hotel", Einnahme: "", Ausgabe: konzert.unterkunft.roomsTotalEUR ?? 0 };
-  const hotelTransportRow = { Art: "Hotel (Transport)", Einnahme: "", Ausgabe: konzert.unterkunft.transportEUR ?? 0 };
+  return veranstaltung.isVermietung
+    ? createExcelDataVermietung(veranstaltung as Vermietung)
+    : createExcelDataKonzert(veranstaltung as Konzert);
 
-  return [
-    titelZeile,
-    {},
-    eintrittRow,
-    reservixRow,
-    barEinnahmenRow,
-    barEinlageRow,
-    zuschussRow,
-    barAusgabenRow,
-    barAnBankRow,
-    gagenRow,
-    dealRow,
-    provisionRow,
-    backlineRockshopRow,
-    technikZumietungRow,
-    fluegelStimmerRow,
-    saalmieteRow,
-    werbung1Row,
-    werbung2Row,
-    werbung3Row,
-    personalRow,
-    tontechnikerRow,
-    lichttechnikerRow,
-    cateringMusikerRow,
-    cateringPersonalRow,
-    hotelRow,
-    hotelTransportRow,
-  ];
-}
+  function createExcelDataKonzert(konzert: Konzert): SimpleExcelRow[] {
+    const kasse = konzert.kasse;
+    const kosten = konzert.kosten;
+    const staff = konzert.staff;
+    const kalk = new KonzertKalkulation(konzert);
 
-export function createExcelDataVermietung({ vermietung, optionen }: { vermietung: Vermietung; optionen: OptionValues }) {
-  const kosten = vermietung.kosten;
-  const klavierStimmerStandard = vermietung.technik.fluegel ? optionen.preisKlavierstimmer : 0;
+    const titelZeile = { Art: konzert.kopf.titel + " - " + konzert.startDatumUhrzeit.mitUhrzeitNumerisch };
+    // Einnahmen
+    const eintrittRow = {
+      Art: `Eintritt (${kasse.istFreigegeben ? "" : "nicht "}freigegeben)`,
+      Einnahme: kasse.einnahmeTicketsEUR ?? 0,
+      Ausgabe: "",
+    };
+    const reservixRow = {
+      Art: "Eintritt Reservix",
+      Einnahme: kasse.einnahmenReservix ?? 0,
+      Ausgabe: "",
+    };
+    const barEinnahmenRow = { Art: "Bar Einnahmen", Einnahme: kasse.einnahmeOhneBankUndTickets ?? 0, Ausgabe: "" };
+    const barEinlageRow = { Art: "Bar Einlage", Einnahme: kasse.einnahmeBankEUR ?? 0, Ausgabe: "" };
+    const zuschussRow = { Art: "Zuschüsse", Einnahme: konzert.eintrittspreise.zuschuss ?? 0, Ausgabe: "" };
+    // Ausgaben
+    const barAusgabenRow = { Art: "Barausgaben", Einnahme: "", Ausgabe: kasse.ausgabenOhneGage ?? 0 };
+    const barAnBankRow = { Art: "Bar an Bank", Einnahme: "", Ausgabe: kasse.ausgabeBankEUR ?? 0 };
 
-  const titelZeile = { Art: vermietung.kopf.titel + " - " + vermietung.startDatumUhrzeit.mitUhrzeitNumerisch };
+    const gagenRow = { Art: "Gagen", Einnahme: "", Ausgabe: kosten.gagenTotalEUR ?? 0 };
+    const dealRow = { Art: "Gagen (Deal)", Einnahme: "", Ausgabe: kalk.dealAbsolutEUR ?? 0 };
+    const provisionRow = { Art: "Provision Agentur", Einnahme: "", Ausgabe: kosten.provisionAgentur ?? 0 };
+    const backlineRockshopRow = { Art: "Backline Rockshop", Einnahme: "", Ausgabe: kosten.backlineEUR ?? 0 };
+    const technikZumietungRow = { Art: "Technik Zumietung", Einnahme: "", Ausgabe: kosten.technikAngebot1EUR ?? 0 };
+    const fluegelStimmerRow = {
+      Art: "Flügelstimmer",
+      Einnahme: "",
+      Ausgabe: kosten.fluegelstimmerEUR || klavierStimmerStandard,
+    };
+    const saalmieteRow = { Art: "Saalmiete", Einnahme: "", Ausgabe: kosten.saalmiete ?? 0 };
+    const werbung1Row = { Art: kosten.werbung1Label, Einnahme: "", Ausgabe: kosten.werbung1 ?? 0 };
+    const werbung2Row = { Art: kosten.werbung2Label, Einnahme: "", Ausgabe: kosten.werbung2 ?? 0 };
+    const werbung3Row = { Art: kosten.werbung3Label, Einnahme: "", Ausgabe: kosten.werbung3 ?? 0 };
+    const personalRow = { Art: "Personal (unbar)", Einnahme: "", Ausgabe: kosten.personal ?? 0 };
+    const tontechnikerRow = {
+      Art: "Tontechniker",
+      Einnahme: "",
+      Ausgabe: !staff.technikerVNotNeeded && !kosten.tontechniker ? "N/A" : kosten.tontechniker,
+    };
+    const lichttechnikerRow = {
+      Art: "Lichttechniker",
+      Einnahme: "",
+      Ausgabe: !staff.technikerNotNeeded && !kosten.lichttechniker ? "N/A" : kosten.lichttechniker,
+    };
+    const cateringMusikerRow = { Art: "Catering (Musiker)", Einnahme: "", Ausgabe: kosten.cateringMusiker ?? 0 };
+    const cateringPersonalRow = { Art: "Catering (Personal)", Einnahme: "", Ausgabe: kosten.cateringPersonal ?? 0 };
+    const hotelRow = { Art: "Hotel", Einnahme: "", Ausgabe: konzert.unterkunft.roomsTotalEUR ?? 0 };
+    const hotelTransportRow = { Art: "Hotel (Transport)", Einnahme: "", Ausgabe: konzert.unterkunft.transportEUR ?? 0 };
 
-  // Einnahmen
-  const saalmieteRow = {
-    Art: "Saalmiete",
-    Einnahme: vermietung.saalmiete ?? 0,
-    Ausgabe: "",
-  };
+    return [
+      titelZeile,
+      eintrittRow,
+      reservixRow,
+      barEinnahmenRow,
+      barEinlageRow,
+      zuschussRow,
+      barAusgabenRow,
+      barAnBankRow,
+      gagenRow,
+      dealRow,
+      provisionRow,
+      backlineRockshopRow,
+      technikZumietungRow,
+      fluegelStimmerRow,
+      saalmieteRow,
+      werbung1Row,
+      werbung2Row,
+      werbung3Row,
+      personalRow,
+      tontechnikerRow,
+      lichttechnikerRow,
+      cateringMusikerRow,
+      cateringPersonalRow,
+      hotelRow,
+      hotelTransportRow,
+    ];
+  }
 
-  // Ausgaben
-  const gagenRow = { Art: "Gagen", Einnahme: "", Ausgabe: kosten.gagenTotalEUR ?? 0 };
-  const backlineRockshopRow = { Art: "Backline Rockshop", Einnahme: "", Ausgabe: kosten.backlineEUR ?? 0 };
-  const technikZumietungRow = { Art: "Technik Zumietung", Einnahme: "", Ausgabe: kosten.technikAngebot1EUR ?? 0 };
-  const fluegelStimmerRow = { Art: "Flügelstimmer", Einnahme: "", Ausgabe: kosten.fluegelstimmerEUR || klavierStimmerStandard };
-  const werbung1Row = { Art: kosten.werbung1Label, Einnahme: "", Ausgabe: kosten.werbung1 ?? 0 };
-  const werbung2Row = { Art: kosten.werbung2Label, Einnahme: "", Ausgabe: kosten.werbung2 ?? 0 };
-  const werbung3Row = { Art: kosten.werbung3Label, Einnahme: "", Ausgabe: kosten.werbung3 ?? 0 };
-  const personalRow = { Art: "Personal (unbar)", Einnahme: "", Ausgabe: kosten.personal ?? 0 };
+  function createExcelDataVermietung(vermietung: Vermietung): SimpleExcelRow[] {
+    const kosten = vermietung.kosten;
 
-  return [
-    titelZeile,
-    {},
-    saalmieteRow,
-    gagenRow,
-    backlineRockshopRow,
-    technikZumietungRow,
-    fluegelStimmerRow,
-    werbung1Row,
-    werbung2Row,
-    werbung3Row,
-    personalRow,
-  ];
+    const titelZeile = { Art: vermietung.kopf.titel + " - " + vermietung.startDatumUhrzeit.mitUhrzeitNumerisch };
+
+    // Einnahmen
+    const saalmieteRow = {
+      Art: "Saalmiete",
+      Einnahme: vermietung.saalmiete ?? 0,
+      Ausgabe: "",
+    };
+
+    // Ausgaben
+    const gagenRow = { Art: "Gagen", Einnahme: "", Ausgabe: kosten.gagenTotalEUR ?? 0 };
+    const backlineRockshopRow = { Art: "Backline Rockshop", Einnahme: "", Ausgabe: kosten.backlineEUR ?? 0 };
+    const technikZumietungRow = { Art: "Technik Zumietung", Einnahme: "", Ausgabe: kosten.technikAngebot1EUR ?? 0 };
+    const fluegelStimmerRow = {
+      Art: "Flügelstimmer",
+      Einnahme: "",
+      Ausgabe: kosten.fluegelstimmerEUR || klavierStimmerStandard,
+    };
+    const werbung1Row = { Art: kosten.werbung1Label, Einnahme: "", Ausgabe: kosten.werbung1 ?? 0 };
+    const werbung2Row = { Art: kosten.werbung2Label, Einnahme: "", Ausgabe: kosten.werbung2 ?? 0 };
+    const werbung3Row = { Art: kosten.werbung3Label, Einnahme: "", Ausgabe: kosten.werbung3 ?? 0 };
+    const personalRow = { Art: "Personal (unbar)", Einnahme: "", Ausgabe: kosten.personal ?? 0 };
+
+    return [
+      titelZeile,
+      saalmieteRow,
+      gagenRow,
+      backlineRockshopRow,
+      technikZumietungRow,
+      fluegelStimmerRow,
+      werbung1Row,
+      werbung2Row,
+      werbung3Row,
+      personalRow,
+    ];
+  }
 }
--- application/vue/src/commons/utilityFunctions.ts
+++ application/vue/src/commons/utilityFunctions.ts
@@ -1,11 +1,10 @@
-import Konzert from "jc-shared/konzert/konzert.ts";
-import { utils, writeFileXLSX } from "xlsx";
-import { createExcelData, createExcelDataVermietung } from "jc-shared/excelPreparation/excelFormatters.ts";
-import Vermietung from "jc-shared/vermietung/vermietung.ts";
-import { prepareExcel } from "jc-shared/excelPreparation/excelKumulierer.ts";
-import forEach from "lodash/forEach";
+import writeXlsxFile, { Schema } from "write-excel-file";
 import Veranstaltung from "jc-shared/veranstaltung/veranstaltung.ts";
 import OptionValues from "jc-shared/optionen/optionValues.ts";
+import { excelRows, VeranstaltungExcelRow } from "jc-shared/excelPreparation/excelRowVeranstaltung.ts";
+import { createExcelData, SimpleExcelRow } from "jc-shared/excelPreparation/excelFormatters.ts";
+
+const red = "#e8838a";
 
 const format = new Intl.NumberFormat("de-DE", {
   minimumFractionDigits: 2,
@@ -17,35 +16,114 @@ export function formatToGermanNumberString(amount: number): string {
   return format.format(amount);
 }
 
-export function asExcelKalk({ veranstaltungen, optionen }: { veranstaltungen: Veranstaltung[]; optionen: OptionValues }) {
+export async function asExcelKalk({ veranstaltungen, optionen }: { veranstaltungen: Veranstaltung[]; optionen: OptionValues }) {
   if (veranstaltungen.length < 1) {
     return;
   }
-  const book = utils.book_new();
-  const sheet = utils.json_to_sheet(prepareExcel({ veranstaltungen, optionen }));
-  sheet["!cols"] = [{ wch: 30 }, { wch: 6 }, { wch: 10 }];
-  utils.book_append_sheet(book, sheet, "Übersicht");
+  const schemaUebersicht: Schema<VeranstaltungExcelRow> = [
+    { column: "Datum", type: Date, format: "dd.mm.yyyy", value: (row) => row.datum, width: 12 },
+    { column: "Titel", type: String, value: (row) => row.titel, width: 30 },
+    {
+      column: "Typ",
+      type: String,
+      value: (row) => row.typ,
+      width: 22,
+      getCellStyle: (row) => ({ backgroundColor: row.color }),
+    },
+    { column: "Eintritt Abendkasse Bar", type: Number, format: "#,##0.00", width: 22, value: (row) => row.abendkasse },
+    { column: "Einnahmen Reservix", type: Number, format: "#,##0.00", width: 22, value: (row) => row.reservix },
+    { column: "Bar Einnahmen", type: Number, format: "#,##0.00", width: 22, value: (row) => row.einnahmenBar },
+    { column: "Bar Einlage", type: Number, format: "#,##0.00", width: 22, value: (row) => row.einlageBar },
+    { column: "Zuschüsse", type: Number, format: "#,##0.00", width: 22, value: (row) => row.zuschuss },
+    { column: "Spende", type: Number, format: "#,##0.00", width: 22, value: (row) => row.spende },
+    { column: "Einnahme 1", type: Number, format: "#,##0.00", width: 22, value: (row) => row.einnahme1 },
+    { column: "(Text)", type: String, value: (row) => row.einnahme1Text },
+    { column: "Einnahme 2", type: Number, format: "#,##0.00", width: 22, value: (row) => row.einnahme2 },
+    { column: "(Text)", type: String, value: (row) => row.einnahme2Text },
+    { column: "Saalmiete", type: Number, format: "#,##0.00", width: 22, value: (row) => row.saalmiete },
+    { column: "Barausgaben", type: Number, format: "#,##0.00", width: 22, value: (row) => row.ausgabenBar },
+    { column: "Bar an Bank", type: Number, format: "#,##0.00", width: 22, value: (row) => row.anBank },
+    { column: "Gagen", type: Number, format: "#,##0.00", width: 22, value: (row) => row.gage },
+    { column: "Gagen (Deal)", type: Number, format: "#,##0.00", width: 22, value: (row) => row.deal },
+    { column: "Provision Agentur", type: Number, format: "#,##0.00", width: 22, value: (row) => row.provision },
+    { column: "Backline Rockshop", type: Number, format: "#,##0.00", width: 22, value: (row) => row.backline },
+    { column: "Technik Zumietung", type: Number, format: "#,##0.00", width: 22, value: (row) => row.technik },
+    { column: "Flügelstimmer", type: Number, format: "#,##0.00", width: 22, value: (row) => row.fluegel },
+    { column: "Personal", type: Number, format: "#,##0.00", width: 22, value: (row) => row.personal },
+    { column: "Hotel", type: Number, format: "#,##0.00", width: 22, value: (row) => row.hotel },
+    { column: "Hotel (Transport)", type: Number, format: "#,##0.00", width: 22, value: (row) => row.hotelTransport },
+    {
+      column: "Tontechniker",
+      type: Number,
+      format: "#,##0.00",
+      width: 22,
+      value: (row) => (row.tontechniker === "N/A" ? 0 : row.tontechniker),
+      getCellStyle: (row) => {
+        return row.tontechniker === "N/A" ? { backgroundColor: red } : undefined;
+      },
+    },
+    {
+      column: "Lichttechniker",
+      type: Number,
+      format: "#,##0.00",
+      width: 22,
+      value: (row) => (row.lichttechniker === "N/A" ? 0 : row.lichttechniker),
+      getCellStyle: (row) => {
+        return row.tontechniker === "N/A" ? { backgroundColor: red } : undefined;
+      },
+    },
+    { column: "Catering Musiker", type: Number, format: "#,##0.00", width: 22, value: (row) => row.cateringMusiker },
+    { column: "Catering Personal", type: Number, format: "#,##0.00", width: 22, value: (row) => row.cateringPersonal },
+    { column: "KSK", type: Number, format: "#,##0.00", width: 22, value: (row) => row.ksk },
+    { column: "GEMA", type: Number, format: "#,##0.00", width: 22, value: (row) => row.gema },
+    { column: "Werbung 1", type: Number, format: "#,##0.00", width: 22, value: (row) => row.werbung1 },
+    { column: "(Text)", type: String, value: (row) => row.werbung1Text },
+    { column: "Werbung 2", type: Number, format: "#,##0.00", width: 22, value: (row) => row.werbung2 },
+    { column: "(Text)", type: String, value: (row) => row.werbung2Text },
+    { column: "Werbung 3", type: Number, format: "#,##0.00", width: 22, value: (row) => row.werbung3 },
+    { column: "(Text)", type: String, value: (row) => row.werbung3Text },
+    { column: "Werbung 4", type: Number, format: "#,##0.00", width: 22, value: (row) => row.werbung4 },
+    { column: "(Text)", type: String, value: (row) => row.werbung4Text },
+    { column: "Werbung 5", type: Number, format: "#,##0.00", width: 22, value: (row) => row.werbung5 },
+    { column: "(Text)", type: String, value: (row) => row.werbung5Text },
+    { column: "Werbung 6", type: Number, format: "#,##0.00", width: 22, value: (row) => row.werbung6 },
+    { column: "(Text)", type: String, value: (row) => row.werbung6Text },
+  ];
+  const uebersichtRows = excelRows({ veranstaltungen, optionen });
 
-  forEach(veranstaltungen, (ver) => {
-    const rows = ver.isVermietung
-      ? createExcelDataVermietung({ vermietung: ver as Vermietung, optionen })
-      : createExcelData({ konzert: ver as Konzert, optionen });
-    const sheet = utils.json_to_sheet(rows);
-    sheet["!cols"] = [{ wch: 30 }, { wch: 6 }, { wch: 10 }];
-    utils.book_append_sheet(
-      book,
-      sheet,
-      `${ver?.startDatumUhrzeit.tagMonatJahrKompakt}-${ver?.kopf?.titel}`
-        .replace(/\s/g, "-")
-        .replace(/\//g, "-")
-        .replace(/[^a-zA-Z0-9\- _]/g, "")
-        .slice(0, 30) || "data",
-    );
-  });
   const erste = veranstaltungen[0];
   const letzte = veranstaltungen[veranstaltungen.length - 1];
   const von = erste.startDatumUhrzeit.tagMonatJahrKompakt;
   const bis = letzte.startDatumUhrzeit.tagMonatJahrKompakt;
-  const vonBisString = von === bis ? von : von + "-" + bis;
-  writeFileXLSX(book, `Kalkulation-${vonBisString}.xlsx`);
+  const vonBisString = von === bis ? von : `${von}-${bis}`;
+
+  await writeXlsxFile(uebersichtRows, {
+    schema: schemaUebersicht,
+    fileName: `Kalkulation-${vonBisString}.xlsx`,
+    stickyColumnsCount: 3,
+  });
+}
+
+export async function asExcelKalkSingle({ veranstaltung, optionen }: { veranstaltung: Veranstaltung; optionen: OptionValues }) {
+  const schemaSingle: Schema<SimpleExcelRow> = [
+    { column: "Art", type: String, value: (row) => row.Art, width: 30 },
+    { column: "Einnahme", type: Number, format: "#,##0.00", value: (row) => row.Einnahme },
+    {
+      column: "Ausgabe",
+      type: Number,
+      format: "#,##0.00",
+      value: (row) => (row.Ausgabe === "N/A" ? 0 : row.Ausgabe),
+      getCellStyle: (row) => {
+        return row.Ausgabe === "N/A" ? { backgroundColor: red } : undefined;
+      },
+    },
+  ];
+
+  const rows = createExcelData({ veranstaltung, optionen });
+
+  await writeXlsxFile(rows, {
+    schema: schemaSingle,
+    fileName: `Kalkulation-${veranstaltung.kopf.titel}.xlsx`,
+    stickyColumnsCount: 3,
+  });
 }
