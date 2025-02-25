82f1f7400bd64e86d6349769bd3e8c21044475c9
leider <derleider@web.de>
2025-02-25 20:59:22 +0100

    handle ellipsis in menu better

--- application/vue/src/components/content/menu/JazzHeader.tsx
+++ application/vue/src/components/content/menu/JazzHeader.tsx
@@ -135,6 +135,7 @@ export function JazzHeader({ activeElement }: { activeElement: string }) {
             selectedKeys={[activeElement]}
             style={{ flex: "auto", minWidth: 0, flexGrow: 2 }}
           />
+          <div style={{ width: 20 }}></div>
           <Menu theme="dark" mode="horizontal" items={userMenu ? [userMenu] : []} selectedKeys={[activeElement]} />
         </ConfigProvider>
       </div>
