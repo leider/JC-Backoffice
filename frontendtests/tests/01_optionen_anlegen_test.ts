2c6afacc7cc4950de33b30b722d9ebc63a3fe680
Stefan Rinderle <stefan@rinderle.info>
2025-04-25 12:31:06 +0200

    verify orte in store and table add orte page object

--- frontendtests/tests/01_optionen_anlegen_test.ts
+++ frontendtests/tests/01_optionen_anlegen_test.ts
@@ -1,26 +1,45 @@
+import { OrtePage } from "../pages/OrtePage";
+
 Feature("Optionen anlegen");
 
-Before(({ login }) => {
+Before(({ I, login }) => {
   login("admin");
-});
 
-Scenario("Erzeuge Typen und Orte", ({ I }) => {
   I.deleteObjectInCollection("optionenstore", "instance");
   I.deleteObjectInCollection("optionenstore", "orte");
+});
+
+Scenario("Erzeuge Optionen", async ({ I }) => {
   I.amOnPage("/vue/optionen");
   I.fillField("Kooperationen", "UI Test\n");
   I.click("Speichern");
 
+  const resOptionStore = await I.loadObjectInCollection(
+    "optionenstore",
+    "instance",
+  );
+  I.assertDeepEqual(resOptionStore.kooperationen, ["UI Test"]);
+});
+
+Scenario("Orte erzeugen und ändern", async ({ I }) => {
+  const page = new OrtePage();
+
+  const expectedName = "Jazzclub";
+  const expectedFlaeche = 300;
+  const expectedPresseName = "JazzclubPresseName";
+  const expectedPresseIn = "Im Jazzclub";
+
+  let expectedOrt = {
+    name: expectedName,
+    flaeche: expectedFlaeche,
+    pressename: expectedPresseName,
+    presseIn: expectedPresseIn,
+  };
+
   I.amOnPage("/vue/orte");
-  I.click(`(//button[@data-testid="add-in-table"])`);
-  I.click('div[data-testid="name0"]');
-  I.fillField("#name", "Jazzclub");
-  I.pressKey("Tab");
-  I.fillField("#flaeche", "300");
-  I.pressKey("Tab");
-  I.fillField("#pressename", "Jazzclub");
-  I.pressKey("Tab");
-  I.fillField("#presseIn", "Im Jazzclub");
-  I.pressKey("Enter");
-  I.click("Speichern");
+
+  page.addOrt(expectedOrt);
+
+  await page.verifyOrtInStore(expectedOrt);
+  page.verifyOrtInTable(expectedOrt);
 });
