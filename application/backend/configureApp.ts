e5d3dc6cee1636ceb84689d5333eb2038d9ae752
leider <derleider@web.de>
2026-04-09 15:36:45 +0200

    donT cache entry files

--- application/backend/configureApp.ts
+++ application/backend/configureApp.ts
@@ -64,6 +64,10 @@ export default function (app: express.Express, forDev?: boolean): void {
     express.static(path.join(__dirname, "static"), {
       maxAge: "10h",
       setHeaders: (res, file) => {
+        if (file.endsWith("index.html") || file.endsWith("manifest.webmanifest")) {
+          res.setHeader("Cache-Control", "no-cache");
+          return;
+        }
         if (file.includes("sw.js")) {
           res.setHeader("Cache-Control", "public, max-age=0");
         }
